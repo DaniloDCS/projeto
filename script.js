e12970a555eb64141bb59a87c474facf12c5d17c
Danilo Costa <danillo.ifpb@gmail.com>
2020-11-20 23:56:22 -0300

    Google Copy

--- script.js
+++
@@ -1,8 +0,0 @@
-var divIntroducao = document.getElementById("introducao");
-var divHomepage = document.getElementById("homepage");
-divHomepage.style.display = "none";
-
-setTimeout(() => {
-    divIntroducao.style.display = "none";
-    divHomepage.style.display = "block";
-}, 5000);
